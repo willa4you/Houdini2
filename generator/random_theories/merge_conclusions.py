a6bce5bd70c0be45acfc3f83803e1947d6af9cad
Edoardo Z <edovi@icloud.com>
2022-09-06 14:53:26 +0200

    add plots and analysis random theories

---
+++ generator/random_theories/merge_conclusions.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import collections
+
+
+ours = []
+delores = []
+
+path_delores = "test_theories/conclusions_delores.csv"
+path_ours = f"test_theories/conclusions_delores.csv"
+
+with open(path_delores, "r") as f_d:
+    with open(path_ours, "r") as f_o:
+        delores = [x.lstrip().rstrip().lstrip("[").rstrip("]") for x in f_d.readlines()]
+        ours = [x.lstrip().rstrip().lstrip("[").rstrip("]") for x in f_o.readlines()]
+#Save to file
+data = pd.DataFrame({'delores': delores, 'ours': ours})
+#data.to_csv("./all_conclusions.csv")
+print(data)
+n = 1000
+counter = 0
+
+#print("ciao"[1:-1])
+for i in range(0,1000):
+    delo = [a.lstrip().rstrip().lstrip("\"").rstrip("\"").lstrip().rstrip() for a in delores[i].split(",")]
+    our = [a.lstrip().rstrip().lstrip("\"").rstrip("\"").lstrip().rstrip() for a in ours[i].split(",")]
+    if collections.Counter(delo) != collections.Counter(our):
+        counter += 1
+print(counter/n)
--- generator/random_theories/plot.py
+++ generator/random_theories/plot.py
@@ -4,21 +4,16 @@ import pandas as pd
 
 sns.set_palette("Paired")
 
-data1 = pd.read_csv("times.csv")
-data2 = pd.read_csv("times2.csv")
+data1 = pd.read_csv("times_delores.csv")
+data2 = pd.read_csv("times_ours.csv")
 data = pd.concat([data1, data2], axis=1)
 print(data)
 #data = pd.melt(data, var_name=['Delores', 'Ours'])
 
-#RNNdata = pd.read_csv("CV_RNN," + t + ".csv")
-
-#data=pd.concat([MLPdata, RNNdata])
-
 fig, axes = plt.subplots(nrows=1, ncols=1)
 
-fig.set_size_inches(11,9.5)
+fig.set_size_inches(7,5.5)
 
 sns.boxplot(data=data)
-
 
 plt.savefig("./results.pdf", papertype='A4', bbox_inches='tight')
